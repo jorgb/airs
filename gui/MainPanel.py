697482d4cc478887a81cfa3be4b6036c119ab41f
Jorg <Jorg@SHALE>
2008-03-26 21:09:49 +0100

    - Added a list control to the view - Ignored some files

--- gui/MainPanel.py
+++ gui/MainPanel.py
@@ -2,6 +2,7 @@ import os.path, wx
 import wx.xrc as xrc
 from wx.lib.pubsub import Publisher
 import appcfg, xmlres, viewmgr
+from SeriesListCtrl import SeriesListCtrl
 
 class MainPanel(wx.Panel):
 
@@ -18,9 +19,21 @@ class MainPanel(wx.Panel):
         self._log_window = xrc.XRCCTRL(self, "ID_LOG_WINDOW")
         self._update_all = xrc.XRCCTRL(self, "ID_BUTTON1")
 
+        # put the mixin control in place and initialize the
+        # columns and styles
+        self._list_panel = xrc.XRCCTRL(self, "ID_LIST_MIXIN")
+        self._series_list = SeriesListCtrl(self._list_panel)
+
+        sizer = wx.BoxSizer()
+        sizer.Add(self._series_list, 1, wx.EXPAND)
+        self._list_panel.SetSizer(sizer)        
+
+        self._series_list.InsertColumn(0, "Episode")
+        self._series_list.InsertColumn(1, "Title")
+        
         self.Bind(wx.EVT_BUTTON, self._onUpdateAll, self._update_all)
         Publisher().subscribe(self._onSignalLogMessage, viewmgr.SIGNAL_APP_LOG)
-
+        
 
     def _onUpdateAll(self, event):
         """
