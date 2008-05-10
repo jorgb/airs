69715ac555a3000645b278e0d22298cf6216d429
Jorgen Bodde <jorg@buckyball>
2008-05-10 19:44:41 +0200

    - Added desktop icon - Installed in /usr/share/apps/airs instead of /usr/share

--- create-deb.sh
+++ create-deb.sh
@@ -9,11 +9,18 @@ find -name "*.bak" -exec rm '{}' \;
 rm -rf build
 mkdir build
 
+rm -f setup/*.deb
+rm -f setup/*.changes
+rm -f setup/*.dsc
+
 cp airs.py build
 cp airs.ico build
 cp -r gui build
 cp -r data build
 cp -r doc build
+cp setup/ubuntu/airs build
+cp setup/ubuntu/airs-icon.png build
+cp setup/ubuntu/airs.desktop build
 
 cd build 
 tar -czf ../deb.tar.gz *
@@ -24,8 +31,8 @@ rm -rf build
 mkdir build
 
 cd build
-mkdir airs-1.0
-cd airs-1.0
+mkdir airs-1.1
+cd airs-1.1
 
 mv ../../deb.tar.gz .
 tar -zxf deb.tar.gz
@@ -37,7 +44,13 @@ rm debian/*.ex
 rm debian/README
 rm debian/README.Debian
 
-cp ../../setup/ubuntu/* debian
+cp -f ../../setup/ubuntu/changelog debian
+cp -f ../../setup/ubuntu/compat debian
+cp -f ../../setup/ubuntu/control debian
+cp -f ../../setup/ubuntu/copyright debian
+cp -f ../../setup/ubuntu/dirs debian
+cp -f ../../setup/ubuntu/docs debian
+cp -f ../../setup/ubuntu/rules debian
 
 sudo dpkg-buildpackage -d
 
