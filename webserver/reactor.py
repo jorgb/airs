6a436055bb132d88d1b2b4537369843691482f26
Jorgen Bodde <jorg@buckyball>
2008-06-01 22:16:33 +0200

    - begin of displaying reasonable data

--- webserver/reactor.py
+++ webserver/reactor.py
@@ -1,23 +1,45 @@
-#!/usr/bin/python
 from threading import Thread
 
 from twisted.web import resource, static, server, twcgi
 from twisted.internet import reactor
 from Queue import Queue
+import synccmd
 
 msg_queue = Queue()
+in_queue = Queue()
+
+#parent = None
+#def send_command(cmd, args):
+#    callID = synccmd.getCallID()
+#    if parent is not None:
+#        evt = synccmd.SyncCallbackCommandEvent(id = callID, cmd = cmd, args = args)
+#        parent.AddPendingEvent(evt)
+#        
+#        # now wait for a response (5 secs)
+#        synccmd.waitForResponse(in_queue, 5000)
 
 def report(msg):
     msg_queue.put(msg)
         
-class series_sel(resource.Resource):
+
+class series_http_handler(resource.Resource):
     def __init__(self):
         self.tel = 0
 
     def render_GET(self, request):
-        report("Received: %s" % request)
+        if not request.args:
+            # HIER MOET IK:
+            # - De GUI vertellen dat ie data moet gaan verzamelen
+            # - Op een of andere manier moet wachten op die data
+            # - Als het er is, aan twisted terug geven zodat ik die op het scherm kan tonen
+            pass
+        
+        else:
+            if "id" in request.args:
+                id = request.args["id"][0]
+                request.write("Series ID: %s" % id)
+   
         self.tel = self.tel + 1
-        request.write("hee. tel: " + str(self.tel))
         return ""
     
     
@@ -37,7 +59,7 @@ class WebServerThread(Thread):
         #root = static.File("c:\www")
         #root.indexNames=['index.html','index.htm']        
         
-        root.putChild("series_sel", series_sel())
+        root.putChild("series", series_http_handler())
         
         port = 8000
         succes = False
